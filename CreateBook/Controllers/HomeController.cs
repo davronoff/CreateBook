2573eb4f56087f6ecbd195f43d037f48b889cf39
csharpfirstgroup <davronoff123gmail.com>
2022-12-12 15:48:05 +0500

    Add project files.

---
+++ CreateBook/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using CreateBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CreateBook.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<Book> books = new List<Book>
+            {
+                new Book
+                {
+                    Id = 1,
+                    Name = "Mexrobdan Chayon",
+                    Author = "Abdulla Qodir",
+                    Price = 100000
+                },
+                new Book
+                {
+                    Id = 2,
+                    Name = "Mexrobdan Chayon",
+                    Author = "Abdulla Qodir",
+                    Price = 100000
+                },
+                new Book
+                {
+                    Id = 3,
+                    Name = "Mexrobdan Chayon",
+                    Author = "Abdulla Qodir",
+                    Price = 100000
+                },
+                new Book
+                {
+                    Id = 4,
+                    Name = "Outliers",
+                    Author = "Taleb Nassim",
+                    Price = 100000
+                },
+            };
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(books);
+        }
+        public IActionResult Book()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Book request)
+        {
+            books.Add(request);
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ CreateBook/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace CreateBook.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public double Price { get; set; }
+    }
+}
